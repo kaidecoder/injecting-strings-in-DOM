170028b6dd99173994757f1a1ecfb4de6149cecb
kaidecoder <131601358+kaidecoder@users.noreply.github.com>
2024-01-02 02:08:03 -0500

    first commit

---
+++ string.js
@@ -0,0 +1,8 @@
+let main = document.querySelector("main")
+
+let strHTML1 = `<p>1.  This is the <a href="#">String</a> with HTML.</p>`
+let strHTML2 = `<p>1.  This is the <a href="#">String</a> with HTML.</p>`
+let strHTML3 = `<p>1.  This is the <a href="#">String</a> with HTML.</p>`
+let strHTML4 = `<p>1.  This is the <a href="#">String</a> with HTML.</p>`
+let strHTML5 = `<p>1.  This is the <a href="#">String</a> with HTML.</p>`
+let strHTML6 = `<p>1.  This is the <a href="#">String</a> with HTML.</p>`
